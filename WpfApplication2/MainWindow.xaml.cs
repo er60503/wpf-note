41de69cfa1a8b5634b7fbfc3db527325427e2237
m305-10\w401 <er6050378@gmail.com>
2018-06-14 16:31:18 +0800

    finish

---
+++ WpfApplication2/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication2
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+   
+        public partial class MainWindow : Window
+        {
+            //紀錄檔案路徑
+            string File_Path = "";
+            public MainWindow()
+            {
+                InitializeComponent();
+            }
+
+            private void New_Click(object sender, RoutedEventArgs e)
+            {
+                //將Textbox清空
+                TextArea.Text = "";
+
+                //開啟儲存檔案視窗
+                Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
+                Nullable<bool> result = dig.ShowDialog();
+
+                if (result == true)
+                {
+                    File_Path = dig.FileName;
+                    System.IO.File.WriteAllText(File_Path, TextArea.Text);
+                }
+
+                //開啟檔案
+                TextArea.Text = System.IO.File.ReadAllText(dig.FileName);
+                Title = dig.SafeFileName;
+            }
+
+            private void Open_Click_1(object sender, RoutedEventArgs e)
+            {
+                //開啟檔案視窗
+                Microsoft.Win32.OpenFileDialog dig = new Microsoft.Win32.OpenFileDialog();
+                Nullable<bool> result = dig.ShowDialog();
+
+                if (result == true)
+                {
+                    File_Path = dig.FileName;
+                    TextArea.Text = System.IO.File.ReadAllText(dig.FileName);
+                }
+
+                Title = dig.SafeFileName;
+            }
+
+            private void Save_Click_2(object sender, RoutedEventArgs e)
+            {
+            //假設儲存路徑空白
+                if (File_Path == "")
+                {
+                    //則另存新檔
+                    Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
+                    Nullable<bool> result = dig.ShowDialog();
+
+                    if (result == true)
+                    {
+                        File_Path = dig.FileName;
+                        System.IO.File.WriteAllText(File_Path, TextArea.Text);
+                    }
+
+                    Title = dig.SafeFileName;
+                }
+                else
+                {
+                    //不然直接儲存
+                    System.IO.File.WriteAllText(File_Path, TextArea.Text);
+                }
+            }
+
+            //改變字體大小
+            private void A10_Click(object sender, RoutedEventArgs e)
+            {
+                TextArea.FontSize = 10;
+            }
+
+            private void A14_Click(object sender, RoutedEventArgs e)
+            {
+                TextArea.FontSize = 14;
+            }
+
+            private void A18_Click(object sender, RoutedEventArgs e)
+            {
+                TextArea.FontSize = 18;
+            }
+
+            private void Save_as_Click(object sender, RoutedEventArgs e)
+            {
+                Microsoft.Win32.SaveFileDialog dig = new Microsoft.Win32.SaveFileDialog();
+                Nullable<bool> result = dig.ShowDialog();
+
+                if (result == true)
+                {
+                    File_Path = dig.FileName;
+                    System.IO.File.WriteAllText(File_Path, TextArea.Text);
+                }
+
+                Title = dig.SafeFileName;
+            }
+
+            private void RadioButton_Checked(object sender, RoutedEventArgs e)
+            {
+                //將背景顏色改為灰，字體顏色為深灰
+                TextArea.Background = Brushes.DarkGray;
+                TextArea.Foreground = Brushes.Gray;
+            }
+
+            private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+            {
+                //將背景顏色改為白，字體顏色為深灰
+                TextArea.Background = Brushes.White;
+                TextArea.Foreground = Brushes.DarkGray;
+            }
+        }
+    }
+
